d8fa01a520c129acb2d3afa878b2c8667a889bd5
BjoernSchilberg <bjoern.schilberg@intevation.de>
2020-07-15 20:52:15 +0000

    Better close handling

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -3,89 +3,91 @@
     <v-dialog
       v-model="dialog"
       persistent
+      scrollable
       max-width="800">
       <v-card>
-        <v-container>
-          <v-img
-            v-if="foto"
-            :aspect-ratio="4.00/1"
-            :src="einsatzstelle.foto" />
-          <v-row
-            align="end"
-            class="lightbox pa-2 fill-height">
-            <v-col>
-              <div
-                v-if="einsatzstelle.titel"
-                class="headline">
-                <strong>{{ einsatzstelle.titel }}</strong>
-              </div>
-              <div
-                v-if="einsatzstelle.datum"
-                class="body-1">
-                Erfasst am: <strong>{{ einsatzstelle.datum }}</strong>
-              </div>
-            </v-col>
-          </v-row>
-          <v-layout row>
-            <v-flex md6>
-              <v-card-text>
-                <strong> Eckdaten </strong>
-                <ul>
-                  <li v-if="einsatzstelle.flaeche">
-                    Ungefähre Flächengröße in m²:
-                    <strong>{{ einsatzstelle.flaeche }}</strong>
-                  </li>
-                </ul>
-              </v-card-text>
-
-              <v-card-text v-if="einsatzstelle.kurzbeschreibung">
-                <strong>Kurzbeschreibung</strong>
-                <br>
-                {{ einsatzstelle.kurzbeschreibung }}
-              </v-card-text>
-            </v-flex>
-            <v-flex md6>
-              <v-card-text
-                v-if="einsatzstelle.institution_ansprechparter || einsatzstelle.vorname_ansprechpartner">
-                <strong>Ansprechpartner</strong>
-                <v-list dense>
-                  <v-list-item v-if="einsatzstelle.institution_ansprechparter">
-                    <v-list-item-icon>
-                      <v-icon color="#0068b4">
-                        mdi-home
-                      </v-icon>
-                    </v-list-item-icon>
-
-                    <v-list-item-content>
-                      <v-list-item-title>{{ einsatzstelle.institution_ansprechparter }}</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item v-if="einsatzstelle.vorname_ansprechpartner">
-                    <v-list-item-icon>
-                      <v-icon color="#0068b4">
-                        mdi-account
-                      </v-icon>
-                    </v-list-item-icon>
-
-                    <v-list-item-content>
-                      <v-list-item-title>{{ einsatzstelle.vorname_ansprechpartner }}</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-card-text>
-            </v-flex>
-          </v-layout>
-        </v-container>
-
-        <v-card-actions class="justify-center">
+        <v-app-bar
+          flat
+          color="white">
+          <v-toolbar-title
+            v-if="einsatzstelle.titel"
+            class="headline">
+            <strong>{{ einsatzstelle.titel }}</strong>
+          </v-toolbar-title>
+          <v-spacer />
           <v-btn
-            small
-            color="#0068b4"
+            fab
+            icon
             dark
+            color="primary"
+            small
             @click="schliessen">
-            Schließen
+            <v-icon>mdi-close</v-icon>
           </v-btn>
-        </v-card-actions>
+        </v-app-bar>
+        <v-sheet
+          class="overflow-y-auto"
+          max-height="600">
+          <v-container>
+            <v-img
+              v-if="foto"
+              :aspect-ratio="4.00/1"
+              :src="einsatzstelle.foto" />
+            <v-layout row>
+              <v-flex md6>
+                <v-card-text>
+                  <strong> Eckdaten </strong>
+                  <ul>
+                    <li
+                      v-if="einsatzstelle.datum">
+                      Erfasst am: <strong>{{ einsatzstelle.datum }}</strong>
+                    </li>
+                    <li v-if="einsatzstelle.flaeche">
+                      Ungefähre Flächengröße in m²:
+                      <strong>{{ einsatzstelle.flaeche }}</strong>
+                    </li>
+                  </ul>
+                </v-card-text>
+
+                <v-card-text v-if="einsatzstelle.kurzbeschreibung">
+                  <strong>Kurzbeschreibung</strong>
+                  <br>
+                  {{ einsatzstelle.kurzbeschreibung }}
+                </v-card-text>
+              </v-flex>
+              <v-flex md6>
+                <v-card-text
+                  v-if="einsatzstelle.institution_ansprechparter || einsatzstelle.vorname_ansprechpartner">
+                  <strong>Ansprechpartner</strong>
+                  <v-list dense>
+                    <v-list-item v-if="einsatzstelle.institution_ansprechparter">
+                      <v-list-item-icon>
+                        <v-icon color="#0068b4">
+                          mdi-home
+                        </v-icon>
+                      </v-list-item-icon>
+
+                      <v-list-item-content>
+                        <v-list-item-title>{{ einsatzstelle.institution_ansprechparter }}</v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item v-if="einsatzstelle.vorname_ansprechpartner">
+                      <v-list-item-icon>
+                        <v-icon color="#0068b4">
+                          mdi-account
+                        </v-icon>
+                      </v-list-item-icon>
+
+                      <v-list-item-content>
+                        <v-list-item-title>{{ einsatzstelle.vorname_ansprechpartner }}</v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list>
+                </v-card-text>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-sheet>
       </v-card>
     </v-dialog>
   </v-row>
